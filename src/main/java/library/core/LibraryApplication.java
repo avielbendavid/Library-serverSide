e69f85f1565c43e9ef9fd38e7524c6b2d5db6a57
Aviel <avielbendavid22@gmail.com>
2021-03-07 16:17:33 +0200

    update AdminService

--- src/main/java/library/core/LibraryApplication.java
+++ src/main/java/library/core/LibraryApplication.java
@@ -2,14 +2,18 @@ package library.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import library.core.services.AdminService;
 
 
 @SpringBootApplication
 public class LibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryApplication.class, args);
-
+		ConfigurableApplicationContext ctx = SpringApplication.run(LibraryApplication.class, args);
+		AdminService adminService = ctx.getBean(AdminService.class);
+		adminService.f();
 	}
 
 }
--- src/main/java/library/core/services/AdminService.java
+++ src/main/java/library/core/services/AdminService.java
@@ -1,5 +1,28 @@
 package library.core.services;
 
-public class AdminService {
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import library.core.exceptions.LibrarySystemException;
+
+@Service
+@Transactional
+public class AdminService extends ClientService {
+
+	private String email;
+	private String password;
+
+	public AdminService(@Value("${adminEmail}") String email, @Value("${adminPassword}") String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+
+	@Override
+	boolean login(String email, String password) throws LibrarySystemException {
+		return false;
+	}
 
 }
